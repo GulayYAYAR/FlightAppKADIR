cb65234b075b1eed44d5952b142583390eb395e1
aatug <aatug@overstock.com>
2022-12-15 22:07:11 -0500

    third Commit

--- src/test/java/e2e/runner/RunnerClass.java
+++ src/test/java/e2e/runner/RunnerClass.java
@@ -10,8 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features/",
         glue = "e2e/stepDefinitions",
         tags = "@smoke",
-        dryRun = true,
-        monochrome = true,
+        dryRun = false,
         plugin = {
                 "pretty", "html:target/cucumber.html","json:target/cucumber.json",
                 "rerun:target/failed.text"
